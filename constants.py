202c88356bd9f6712aeb129775a4638df1e7b08e
lee Guercin <lguercin@ulf.edu>
2024-11-18 23:47:03 -0500

    added my first comment

---
+++ constants.py
@@ -0,0 +1,5 @@
+boardcol = 640 // 32
+boardrow = 512 // 32
+area = 32
+length = 640
+width = 512
--- whackamole.py
+++ whackamole.py
@@ -1,22 +1,55 @@
 import pygame
+from constants import *
+import random
+
+
+def draw_grid(screen):
+    for i in range(0, boardcol):
+        pygame.draw.line(screen, "dark blue", (i * area, 0), (i * area, width), 2)
+    for i in range(0, boardrow):
+        pygame.draw.line(screen, "dark blue", (0, i * area), (length, i * area), 2)
 
 
 def main():
     try:
         pygame.init()
         # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
+
         mole_image = pygame.image.load("mole.png")
+
         screen = pygame.display.set_mode((640, 512))
+
         clock = pygame.time.Clock()
+
         running = True
+        screen.fill("light blue")
+        draw_grid(screen)
+        screen.blit(mole_image, mole_image.get_rect(topleft=(0, 0)))
         while running:
             for event in pygame.event.get():
+
                 if event.type == pygame.QUIT:
                     running = False
-            screen.fill("light green")
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    a, b = event.pos
+                    if a // 32 == 0 and b // 32 == 0:
+                        screen.fill("light blue")
+                        draw_grid(screen)
+                        x = random.randrange(1, 20)
+                        y = random.randrange(1, 16)
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(x * area, y * area)))
+
+                    if a // 32 == x and b // 32 == y:
+                        screen.fill("light blue")
+                        draw_grid(screen)
+                        x = random.randrange(1, 20)
+                        y = random.randrange(1, 16)
+                        screen.blit(mole_image, mole_image.get_rect(topleft=(x * area, y * area)))
+
             pygame.display.flip()
             clock.tick(60)
+
+
     finally:
         pygame.quit()
 
